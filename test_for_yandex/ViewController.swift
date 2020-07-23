22959ae1caee0eaacb01ec2d4d35028b0bb910fb
bios90 <bios90@mail.ru>
2020-07-23 16:46:18 +0300

    Initial Commit

---
+++ test_for_yandex/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test_for_yandex
+//
+//  Created by Филипп Бесядовский on 23.07.2020.
+//  Copyright © 2020 dimfcompany. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
